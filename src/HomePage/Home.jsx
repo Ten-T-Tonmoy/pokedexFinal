b8c8a41bfb374b08e433e3184ce32071e1214493
Ten-T-Tonmoy <stu01754@gmail.com>
2025-04-01 23:46:15 +0600

    more hassles gone

--- src/HomePage/Home.jsx
+++ src/HomePage/Home.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import Topbar from "./Topbar";
+import Navbar from "./Navbar";
+import SearchBar from "./SearchBar";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="flex flex-col">
+      <Topbar />
+      <Navbar />
+      <SearchBar />
+    </div>
+  );
 };
 
 export default Home;
