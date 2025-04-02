657d1448a8638591262887b1f4048d5907939321
Ten-T-Tonmoy <stu01754@gmail.com>
2025-04-02 23:16:53 +0600

    ocd cost me an hour for this shit

--- src/HomePage/Topbar.jsx
+++ src/HomePage/Topbar.jsx
@@ -1,11 +1,19 @@
 import React from "react";
+import bulb from "../assets/bulb.png";
+import char from "../assets/char.png";
+import ash from "../assets/ash.png";
+import pokeball from "../assets/pokeball.png";
+import pokemonTitle from "../assets/pokemonTitle.png";
 
 const Topbar = () => {
-  return <div>
-    <div>
-      <img src="" alt="" />
+  return (
+    <div className="flex grid-cols-4 gap-2 mx-auto">
+      <div className="h-14 w-[20vw] ">
+        <img src={pokemonTitle} className="h-full" alt="" />
+      </div>
+      <img src={ash} alt="" className="h-14" />
     </div>
-  </div>;
+  );
 };
 
 export default Topbar;
